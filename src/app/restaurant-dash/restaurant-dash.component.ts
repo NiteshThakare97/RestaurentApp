fce0461d8e74e48aa226ac880a4347fc84679878
silent-codder <ajinkyashinde9799@gmail.com>
2022-07-21 12:51:10 +0530

    first commit

---
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { RestaurantData } from './restaurant.model';
+
+@Component({
+  selector: 'app-restaurant-dash',
+  templateUrl: './restaurant-dash.component.html',
+  styleUrls: ['./restaurant-dash.component.css']
+})
+export class RestaurantDashComponent implements OnInit {
+
+formValue!:FormGroup
+restaurantModelObj :RestaurantData = new RestaurantData;
+  constructor(private formBuilder: FormBuilder, private api:ApiService) { }
+
+  ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name:[''],
+      email:[''],
+      mobile:[''],
+      address:[''],
+      services:['']
+    })
+  }
+
+// Now Subscribing our data which is maped via Services..0
+addResto(){
+  this.restaurantModelObj.name = this.formValue.value.name;
+  this.restaurantModelObj.email = this.formValue.value.email;
+  this.restaurantModelObj.mobile = this.formValue.value.mobile;
+  this.restaurantModelObj.address = this.formValue.value.address;
+  this.restaurantModelObj.services = this.formValue.value.services;
+
+  this.api.postRestaurant(this.restaurantModelObj).subscribe(res=>{
+    console.log(res);
+    alert("Restaurent Records Added Successfully");
+  }
+  )
+}
+}
---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http:HttpClient) { }
+  //Now here I will define the POST,GET,PUT,DELETE 
+  //Create restaurant using POST Method
+  postRestaurant(data:any){
+    return this._http.post<any>("http://localhost:3000/posts",data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  //Get Restaurant data using Get Method
+  getRestaurant(){
+    return this._http.get<any>("http://localhost:3000/posts").pipe(map((res:any)=>{
+      return res;
+    }))
+  } 
+  //Update Restaurant using PUT method
+  updateRestaurant(data:any, id:number){
+    return this._http.put<any>("http://localhost:3000/posts/"+id,data).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  //Delete Restaurant using Delete Methods
+  deleteRestaurant(id:number){
+    return this._http.delete<any>("http://localhost:3000/posts/"+id).pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  //This is done guys.. 0 simple
+}
